7b75967d72b595617aaecec7a807479d65a86183
Mounika554-gunda <gvndmounika554@gmail.com>
2026-08-17 12:59:53 +0530

    operators in java

---
+++ src/javaoperators/Arithmeticoperator.java
@@ -0,0 +1,16 @@
+package javaoperators;
+
+public class Arithmeticoperator {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("Addition of two numbers:" +(a+b));
+		System.out.println("difference of two numbers :" +(a-b));
+		System.out.println("Multiplication of two numbers :" +a*b);
+		System.out.println("Division of two numbers:" +a/b);
+		System.out.println("Modulus of two numbers:" +a%b);
+		System.out.println(5*2/3);
+	}
+
+}
---
+++ src/javaoperators/Comparisionoperator.java
@@ -0,0 +1,25 @@
+package javaoperators;
+
+public class Comparisionoperator {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int c = 10;
+		String s1 = "mounika";
+		String s2 = new String("mounika");
+		
+		System.out.println(a==b);
+		System.out.println(b==c);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(b>=c);
+		System.out.println(b<=c);
+		
+		System.out.println(s1==s2);
+
+	}
+
+}
